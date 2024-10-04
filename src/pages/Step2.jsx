99e569bdd7260487fda3f5cc0a739d33c2da6a29
camila-go <camil4.gon@gmail.com>
2024-10-04 18:30:02 -0400

    "Oct4"

---
+++ src/pages/Step2.jsx
@@ -0,0 +1,28 @@
+
+import axios from 'axios';  
+import { useEffect, useState } from 'react';  
+import { Link } from 'react-router-dom';  
+import React from 'react';  
+import '/src/css/step2.css'; 
+
+import React from 'react';  
+import './index.css'; // Assuming you have a CSS file named index.css  
+
+const MyComponent = () => {  
+  return (  
+    <div>  
+      <div className="group-parent">  
+        <div className="s-parent">  
+          <img className="s-icon" alt="" src="S.svg" />  
+          
+          <div className="ync-wrapper">  
+            <img className="ync-icon" alt="" src="ync.svg" />  
+          </div>  
+        </div>  
+        <img className="vector-icon" alt="" src="Vector.svg" />  
+      </div>  
+    </div>  
+  );  
+};  
+
+export default MyComponent;
