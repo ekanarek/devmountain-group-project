8bc0fd1b7ef53bc0a325b1d1b84c912bc15b4a3d
Peter Farrow <pnfarrow@gmail.com>
2024-10-05 13:24:31 -0600

    organizing file structure

--- src/components/TestButton.jsx
+++ src/components/TestButton.jsx
@@ -3,10 +3,7 @@ import axios from "axios";
 export default function TestButton() {
   const login = async () => {
     const res = await axios.get("/login");
-    return res.data;
   };
 
   return <button onClick={login}>Login to Spotify</button>;
 }
-
-//testingggggggg
