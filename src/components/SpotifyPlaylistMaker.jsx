2959541206e4fb1d5cfc87d74ccadbf98698e092
Parker Montgomery <peejmontyw@gmail.com>
2024-10-17 18:04:34 -0600

    More playlist maker

--- src/components/SpotifyPlaylistMaker.jsx
+++ src/components/SpotifyPlaylistMaker.jsx
@@ -18,12 +18,49 @@ const SpotifyPlaylistMaker = () => {
         'grant_type=client_credentials', {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa(client_id + ':' client_secret)
+                'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
             }
         });
         setAccessToken(response.data.access_token);
         };
 
         getAccessToken();
-    }, []
+    }, []);
+
+    const fetchSongs = async () => {
+        const response = await axios.get('https://api.spotify.com/v1/recommendations', {
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+            params: {
+                seed_genres: genre,
+                limit: 10,
+                target_energy: energy,
+                target_instrumentalness: instrumentalness,
+                target_valence: happiness,
+            }
+        });
+        setSongs(response.data.tracks);
+    };
+    return (
+        <div>
+            <h1>Sync Song Fetcher</h1>
+            <div>
+                <label>Select Genre: </label>
+                <select value={genre} onChange={(e) => setGenre(e.target.value)}>
+                    <option value="hip-hop">Hip-Hop</option>
+                    <option value="pop">Pop</option>
+                    <option value="rock">Rock</option>
+                    <option value="jazz">Jazz</option>
+                    {/* figure out what genres we want here :) */}
+                </select>
+            </div>
+
+            <div>
+                <label>Energy!: {energy}</label>
+                {/* Sliders here when I can figure them out why are they so silly */}
+            </div>
+
+        </div>
+    )
 }
